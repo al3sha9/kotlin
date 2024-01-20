0171f9598458ba41531985392a58f9c74a33d6d9
Ali Shan <optimist.alishan@gmail.com>
2024-01-21 04:35:28 +0500

    pushing

--- 1.kt
+++ 1.kt
@@ -1,12 +1,10 @@
-fun main(){
-    val a = 4
-
-    val res = when (a){
-        1-> "123"
-        2-> "123"
-        3-> "123"
-        4-> "number 4"
-        else-> "nami"
+fun Aras(){
+    var cars = arrayOf("NIGGA 1", "NIGGA 2", "NIGGA 3", "NIGGA 4")
+    for(x in cars){
+        println(x)
     }
-    println(res)
+}
+fun main(){
+    Aras()
+
 }
